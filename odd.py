8f7eab2708e277dbedff15ce40aa1745422e2fbe
ClayTaylor <claydevelops@gmail.com>
2021-01-11 06:17:43 -0600

    Adding a For Loop

--- odd.py
+++ odd.py
@@ -4,14 +4,15 @@ odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,  # odds is a list [], but also a vari
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
-right_this_minute = datetime.today().minute  # right_this_minute is a variable and it is an assigned to datetime.today().minute
+for i in range (5):
+    right_this_minute = datetime.today().minute  # right_this_minute is a variable and it is an assigned to datetime.today().minute
 
-if right_this_minute in odds:  # if will evaluate to true or false. 'in' operator checks if one thing is inside another
-    print("This minute seems a little odd.")  # print function displays a standard output
-else:  # if 'if' is false, then else will execute
-    print("Not an odd minute.")
+    if right_this_minute in odds:  # if will evaluate to true or false. 'in' operator checks if one thing is inside another
+        print("This minute seems a little odd.")  # print function displays a standard output
+    else:  # if 'if' is false, then else will execute
+        print("Not an odd minute.")
 
-# There is no 'elseif' in Python, only 'elif'. i can have as many 'elif's as I would like.
+    # There is no 'elseif' in Python, only 'elif'. i can have as many 'elif's as I would like.
 
 checkDay = datetime.today().weekday()  # Create a variable checking the day of the week using the function weekday() from the library datetime, imported at the top of the file
 today = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
